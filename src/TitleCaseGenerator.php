6143ac0baac72070ddd262cee0baa4b22dc306c5
Ben Spenard & Jenna Maloney <student@epicodus.com>
2015-08-10 15:48:02 -0700

    add comments

--- src/TitleCaseGenerator.php
+++ src/TitleCaseGenerator.php
@@ -1,14 +1,15 @@
 <?php
     class TitleCaseGenerator
     {
+//Function to correctly titlecase inputs
         function makeTitleCase($input_title)
         {
-//
+//breaks str into array and turns text into lowercase
             $input_array_of_words = explode(" ", mb_strtolower( $input_title ));
             $output_titlecased = array();
 
             $excluded_words = array('a', 'an', 'and', 'at', 'but', 'by', 'else', 'for', 'from', 'if', 'in', 'into', 'nor', 'of', 'off', 'on', 'or', 'out', 'over', 'the', 'then', 'to', 'when', 'with');
-
+//for each word in the array uppercase the first leter, except for excluded words
             foreach ( $input_array_of_words as $position => $indi ) {
                 if( !$position or !in_array($indi, $excluded_words))
                 $input_array_of_words[$position] = ucfirst($indi);
